1f7de1beb6d7df42e590aacedc2d8068e77347af
Asmita Vadage <asmitaborawake@gmail.com>
2019-07-23 12:15:27 +0530

    Initial Commit

---
+++ LocliseDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocliseDemo
+//
+//  Created by User on 23/07/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
