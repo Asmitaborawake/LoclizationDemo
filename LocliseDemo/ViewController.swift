99c11e705873ce01006b15acec07fdac32bf1461
Asmita Vadage <asmitaborawake@gmail.com>
2019-07-23 12:35:45 +0530

    localisation demo for english and spnish lang

--- LocliseDemo/ViewController.swift
+++ LocliseDemo/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segment: UISegmentedControl!
+    @IBOutlet weak var fname: UILabel!
+    
+    @IBOutlet weak var lname: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    @IBAction func segmentClick(_ sender: Any) {
+        
+        if segment.selectedSegmentIndex == 0 {
+        
+            laungchange(strLan: "en")
+        }else {
+            laungchange(strLan: "es")
+        }
+    }
+    
+    func laungchange(strLan: String) {
+        fname.text = "FirstNameKey".localisableString(loc: strLan)
+        lname.text = "LastNameKey".localisableString(loc: strLan)
+    }
+    
 }
 
+
+extension String {
+    func localisableString(loc: String) -> String {
+        let path = Bundle.main.path(forResource: loc, ofType: "lproj")
+        let bundle = Bundle(path: path!)
+        return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: "", comment: "")
+    }
+}
